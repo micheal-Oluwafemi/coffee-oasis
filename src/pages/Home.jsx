9b738700e743f091e2672e624aad4c0e1735df01
micheal-Oluwafemi <raytech401@gmail.com>
2023-11-24 09:04:24 +0100

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,7 +38,8 @@ const Home = () => {
           <input
             type='text'
             placeholder='Find your Coffee..'
-            value={search}
+            value={search.toLowerCase()}
+            // autoCapitalize='none'
             autoCapitalize='none'
             onChange={(e) => setSearch(e.target.value)}
             className='w-full bg-transparent text-lg font-medium py-2 pl-7 outline-none border-0 text-gray-400 '
